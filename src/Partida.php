1983f05e1e20a792524e3fd6921bcab94c0664b2
Ferrokwastaken <israelduquesilva@hotmail.com>
2024-10-14 17:50:36 +0100

    PartidaTest

---
+++ src/Partida.php
@@ -0,0 +1,36 @@
+<?php
+  namespace Dsw\Rpg;
+
+  class Partida {
+    private $personajes;
+
+    function __construct()
+    {
+      $this->personajes = [];
+    }
+    
+    function agregarPersonaje(Personaje $personaje) {
+      $this->personajes [] = $personaje;
+    }
+
+    function obtenerPersonajes() : array {
+      return $this->personajes;
+    }
+
+    function eliminarPersonaje(Personaje $personaje) {
+      $result = array_search($personaje, $this->personajes);
+
+      if ($result === false) {
+        echo "No se encuentra en el array de personajes";
+      } else {
+        array_splice($this->personajes, $result, 1);
+      }
+    }
+
+    function obtenerPersonajesPorClase($class) : array {
+      return array_filter($this->personajes, function ($personaje) use ($class) {
+        return $personaje instanceof $class;
+      });
+    }
+  }
+?>
